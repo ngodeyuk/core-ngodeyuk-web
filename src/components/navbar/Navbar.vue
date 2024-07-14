fd5722be456374cd56f50c273afa3b1d6955af14
Muhamad Mupid Ahmadiawan <133787157+Yuefii@users.noreply.github.com>
2024-07-14 09:00:09 +0700

    feat: features(navbar) ui for mobile (#4)
    * feat: features(home) hero section ui
    
    * build: setup workflows github
    
    * feat: features(navbar) ui for mobile

--- src/components/navbar/Navbar.vue
+++ src/components/navbar/Navbar.vue
@@ -1,6 +1,16 @@
 <script setup lang="ts">
 import NavLink from "@/components/navbar/NavLink.vue"
 import Button from "@/components/Button.vue";
+import {
+    Drawer,
+    DrawerClose,
+    DrawerContent,
+    DrawerDescription,
+    DrawerFooter,
+    DrawerHeader,
+    DrawerTitle,
+    DrawerTrigger,
+} from '@/packages/ui/drawer';
 const links = [
     { name: 'Features', href: '#features' },
     { name: 'Pricing', href: '#features' },
@@ -15,12 +25,26 @@ const links = [
             <div class="flex h-14 items-center">
                 <div href="#" class="mr-auto flex items-center font-bold">
                     <img class="w-[100px] h-[100px]" src="/logo.png" alt="logo">
-                    <h1 class="text-4xl text-emerald-500">Ngode<span class="text-white">yuk</span></h1>
+                    <h1 class="hidden md:block text-4xl text-emerald-500">Ngode<span class="text-white">yuk</span></h1>
                 </div>
-                <nav class="ml-auto flex items-center space-x-4">
+                <nav class="hidden ml-auto md:flex items-center space-x-4">
                     <NavLink :links="links" />
                     <Button href="#_" title="Download" />
                 </nav>
+                <div class="block md:hidden items-center">
+                    <Drawer>
+                        <DrawerTrigger>
+                            <img class="w-[40px]" src="/icons/menu.svg" alt="">
+                        </DrawerTrigger>
+                        <DrawerContent>
+                            <DrawerTitle class="pt-5 text-2xl text-center">Menu</DrawerTitle>
+                            <DrawerClose class="mt-5 w-1/2 ml-6 text-start text-lg flex flex-col space-y-3">
+                                <NavLink :links="links" />
+                                <Button href="#_" title="Download" />
+                            </DrawerClose>
+                        </DrawerContent>
+                    </Drawer>
+                </div>
             </div>
         </div>
     </nav>
