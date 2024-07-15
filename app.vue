1c8509b16dd020f6638a67be561da6405e44f6af
Muhamad Mupid Ahmadiawan <133787157+Yuefii@users.noreply.github.com>
2024-07-16 05:17:44 +0700

    feat: optimization seo metadata (#11)

--- app.vue
+++ app.vue
@@ -1,3 +1,27 @@
+<script setup lang="ts">
+const title = ref('Ngodeyuk')
+useSeoMeta({
+  title: `${title.value} - Belajar Coding Mudah`,
+  ogTitle: `${title.value} - Belajar Coding Mudah`,
+  description: `${title.value} adalah platform belajar ngoding dengan cara yang menyenangkan dan interaktif.`,
+  ogDescription: `${title.value} adalah platform belajar ngoding dengan cara yang menyenangkan dan interaktif.`,
+  ogImage: 'https://ngodeyuk.site/Favicon.png',
+  twitterCard: 'summary_large_image',
+  keywords: 'Ngodeyuk, belajar coding interaktif, platform belajar ngoding, tutorial pemrograman pemula, kursus coding online, belajar pemrograman mudah, belajar coding, tutorial coding, pemrograman, kuis coding',
+  author: `${title.value} Team`,
+  viewport: 'width=device-width, initial-scale=1',
+  robots: 'index, follow',
+  canonical: 'https://ngodeyuk.site',
+  'og:type': 'website',
+  'og:url': 'https://ngodeyuk.site',
+  'og:site_name': 'Ngodeyuk',
+  'twitter:title': `${title.value} - Belajar Coding Mudah`,
+  'twitter:description': `${title.value} adalah platform belajar ngoding dengan cara yang menyenangkan dan interaktif.`,
+  'twitter:image': 'https://ngodeyuk.site/Favicon.png',
+})
+</script>
+
+
 <template>
   <main class="min-h-screen bg-zinc-900">
     <NuxtLayout>
--- components/homepage/HeroSection.vue
+++ components/homepage/HeroSection.vue
@@ -1,9 +1,15 @@
 <script setup lang="ts">
-import { onMounted } from "vue";
 import Button from "@/components/Button.vue";
 import Typewriter from 'typewriter-effect/dist/core';
 
 const rating = ref(5);
+const images = [
+    "https://yuefii.my.id/profile.jpg",
+    "https://i.pinimg.com/736x/bc/20/94/bc20948f3bccfd926b41688b38b3d9c9.jpg",
+    "https://img.freepik.com/free-photo/beautiful-anime-character-cartoon-scene_23-2151035157.jpg",
+    "https://img.freepik.com/free-photo/anime-style-character-space_23-2151134100.jpg   "
+]
+
 onMounted(() => {
     const target = document.querySelector('.typewriter');
     const typewriter = new Typewriter(target, {
@@ -26,33 +32,26 @@ onMounted(() => {
                 <h1 class="text-4xl text-white md:text-6xl font-medium">Belajar Ngoding <br> <span
                         class="typewriter"></span></h1>
                 <p class="text-justify text-muted-foreground text-md md:text-lg">Apakah Anda seorang pemula dalam dunia
-                    pemrograman
-                    yang ingin belajar dengan cara yang menyenangkan dan interaktif? Ngoding Yuk adalah aplikasi yang
+                    pemrograman yang ingin belajar ngoding dengan cara yang menyenangkan dan interaktif? Ngodeyuk adalah
+                    aplikasi yang
                     tepat untuk Anda!</p>
                 <Button href="#_" title="Download Now" />
                 <div class="flex flex-col md:flex-row items-center pt-5">
                     <div class="flex item-center">
-                        <img class="w-[50px] h-[50px] object-cover -mr-4 rounded-full"
-                            src="https://yuefii.my.id/profile.jpg" alt="">
-                        <img class="w-[50px] h-[50px] object-cover -mr-4 rounded-full"
-                            src="https://i.pinimg.com/736x/bc/20/94/bc20948f3bccfd926b41688b38b3d9c9.jpg" alt="">
-                        <img class="w-[50px] h-[50px] object-cover -mr-4 rounded-full"
-                            src="https://img.freepik.com/free-photo/beautiful-anime-character-cartoon-scene_23-2151035157.jpg"
-                            alt="">
-                        <img class="w-[50px] h-[50px] object-cover -mr-4 rounded-full"
-                            src="https://img.freepik.com/free-photo/anime-style-character-space_23-2151134100.jpg"
-                            alt="">
+                        <div class="relative -mr-3" v-for="(image, index) in images" :key="index">
+                            <img class="w-[50px] h-[50px] object-cover rounded-full" :src="image" :alt="index">
+                        </div>
                     </div>
-                    <div class="md:ml-10 mt-3 md:mt-0 flex flex-col items-center md:items-baseline">
+                    <div class="md:ml-6 mt-3 md:mt-0 flex flex-col items-center md:items-baseline">
                         <RatingStar :rating="rating" />
                         <p class="text-white opacity-90 text-md"><span class="opacity-100 font-bold">126</span> Orang
-                            Menggunakan telah aplikasi ini.
+                            telah menggunakan aplikasi ini.
                         </p>
                     </div>
                 </div>
             </div>
             <div class="hidden lg:block mr-28 mt-3 w-[300px]">
-                <img class="w-full" src="/phone.png" alt="">
+                <img class="w-full" src="/phone.png" alt="Belajar coding interaktif">
             </div>
         </div>
     </main>
